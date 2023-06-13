81c42ac20a33196bf805a6be5c2122446f9d3110
Mrprogramador <mrprogramador@outlook.com>
2023-06-12 20:41:31 -0400

    Atividade: Trabalhando com Variáveis

---
+++ js/trabalhando-variaveis.js
@@ -0,0 +1,11 @@
+// Exercicio decimal
+let decimal = 1.70;
+console.log("Decimal:", decimal);
+
+// Exercicio inteiro
+let inteiro = 28;
+console.log("Inteiro:", inteiro);
+
+// Exercicio string
+let string = "Maicon Rodrigues";
+console.log("String:", string);
