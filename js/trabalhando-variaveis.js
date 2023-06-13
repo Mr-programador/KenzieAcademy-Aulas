8721b7637c3ed6956ce1036ddf46c2cf3d211e81
Mrprogramador <mrprogramador@outlook.com>
2023-06-12 23:29:32 -0400

    Finalizada Atividade: Trabalhando com Variáveis

--- js/trabalhando-variaveis.js
+++ js/trabalhando-variaveis.js
@@ -1,11 +1,53 @@
-// Exercicio decimal
+//Exercicio 1
+
+//Decimal
 let decimal = 1.70;
 console.log("Decimal:", decimal);
 
-// Exercicio inteiro
+//Iinteiro
 let inteiro = 28;
 console.log("Inteiro:", inteiro);
 
-// Exercicio string
+//String
 let string = "Maicon Rodrigues";
 console.log("String:", string);
+
+//Exercicio 2
+
+// Declaração da variável
+let num = 30;
+// Método typeof para verificar o tipo da variável
+console.log (typeof num);
+
+//Exercicio 3
+
+// Declaração da variável
+let boo = true;
+
+// Método typeof para verificar o tipo da variável
+console.log (typeof boo);
+
+//Exercicio 4
+
+// Declaração da variável
+let nome = "Maicon";
+// Método typeof para verificar o tipo da variável
+console.log (typeof nome);
+
+//Exercicio 5
+
+let verdade = true;
+let mentira = false;
+
+console.log(typeof verdade);
+console.log(typeof mentira);
+
+//Exercicio 6
+
+let nulo = null;
+console.log(String(nulo));
+
+//Exercicio 6
+
+let indefinido = undefined;
+console.log(typeof indefinido);
