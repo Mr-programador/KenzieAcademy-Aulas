1ed567753de0e40cebf4621022c92cd3c709eb01
Mrprogramador <mrprogramador@outlook.com>
2023-06-13 00:54:23 -0400

    Finaizada Atividade: Praticando Operadores

---
+++ js/praticando-operadores.js
@@ -0,0 +1,44 @@
+//Exercício 1: Some as duas variáveis e imprima
+let num1 = 5;
+let num2 = 10;
+
+console.log (num1 + num2);
+
+//Exercício 2: Calcule o comprimento da circunferência e imprima
+let pi = 3.14;
+let raio = 5;
+
+console.log (raio * pi);
+
+//Exercício 3: Converta a temperatura de Celsius para Fahrenheit e imprima
+let temperaturaCelsius = 30;
+let conversorFahrenheit = ((temperaturaCelsius * 9)/5) + 32;
+
+console.log(conversorFahrenheit);
+
+//Exercício 4: Calcule a média das duas notas e imprima
+let nota1 = 7;
+let nota2 = 9;
+let media = (nota1 + nota2)/2;
+
+console.log (`A média foi ${media}`);
+
+//Exercício 5: Calcule a área do retângulo e imprima
+let largura = 10;
+let comprimento = 20;
+let area = largura * comprimento;
+
+console.log (`A área é de ${area}m`);
+
+//Exercício 6: Calcule o salário líquido e imprima
+let salarioBruto = 5000;
+let salarioLiquido = salarioBruto - (salarioBruto * 0.1);
+
+console.log (`O salário líquido é de ${salarioLiquido}R$`);
+
+//Exercício 7: Calcule o salário bruto e imprima
+let horasTrabalhadas = 40;
+let valorHora = 50;
+let salarioTotal = horasTrabalhadas * valorHora;
+
+console.log (`O salário bruto é de ${salarioTotal}R$`);
