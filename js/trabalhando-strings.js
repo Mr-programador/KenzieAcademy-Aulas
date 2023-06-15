672925ca5d0f1d4f234d0171bff212e999b17e1f
Mrprogramador <mrprogramador@outlook.com>
2023-06-15 02:35:04 -0400

    Finalizada, Atividade: Trabalhando com Strings

---
+++ js/trabalhando-strings.js
@@ -0,0 +1,32 @@
+//Exercicio 1
+let nome1 ="João";
+let nome2 = "Silva";
+
+console.log(`Olá, ${nome1} ${nome2}!`);
+
+//Exercicio 2
+let string1 = "hello";
+let string2 = "world";
+
+console.log (`${string1}${string2}`);
+
+//Exercicio 3
+
+//parseInt: A função parseInt converte um valor para um número inteiro, ignorando qualquer parte decimal.
+let decimal = 12.5;
+console.log(parseInt(parseFloat(decimal)));
+
+//Math.floor: A função Math.floor arredonda um número para baixo.
+let decimalF = 12.5;
+console.log(Math.floor(decimal));
+
+//Exercicio 4
+let conversorNum = "12";
+console.log(parseInt(conversorNum));
+
+//Exercicio 5
+let num1 = "12";
+let num2 = "3";
+let soma = parseInt(num1) + parseInt(num2);
+
+console.log(soma);
