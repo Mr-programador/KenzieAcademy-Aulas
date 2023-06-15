c7becfe3d92dc2bf5093142f2ec0f9915f87e57b
Mrprogramador <mrprogramador@outlook.com>
2023-06-15 18:42:54 -0400

    Finalizada, Entrega

--- js/taf.js
+++ js/taf.js
@@ -1,39 +1,64 @@
 // Variáveis para aplicar os resultados do teste. Você deverá categoriza-las como let/const, mas não deve alterar os valores.
-let gender = process.argv[2]; // Genero 
-let height = process.argv[3]; // Altura
-let barReps = process.argv[4]; // Repetições com barra
-let barSeconds = process.argv[5]; // Tempo das repetições com barra 
-let abs = process.argv[6]; // Abdominais
-let runDistance = process.argv[7]; // Distância da corrida
-let runTime = process.argv[8]; // Tempo da corrida
-let swimDistance = process.argv[9]; // Distância da natação
-let swimTime = process.argv[10]; // Tempo da natação
-let diveTime = process.argv[11]; // Tempo de mergulho
+const gender = process.argv[2]; // Genero 
+const height = process.argv[3]; // Altura
+const barReps = process.argv[4]; // Repetições com barra
+const barSeconds = process.argv[5]; // Tempo das repetições com barra 
+const abs = process.argv[6]; // Abdominais
+const runDistance = process.argv[7]; // Distância da corrida
+const runTime = process.argv[8]; // Tempo da corrida
+const swimDistance = process.argv[9]; // Distância da natação
+const swimTime = process.argv[10]; // Tempo da natação
+const diveTime = process.argv[11]; // Tempo de mergulho
 
 let passed = false;
 
-// Critérios para aprovação no TAF
-let minBarReps = 10;
-let maxBarSeconds = 30;
-let minAbs = 20;
-let maxRunTime = 20;
-let maxSwimTime = 10;
-let maxDiveTime = 5;
+/*
+    Importante:
+    Se o genero (gender) for masculino, utilize a palavra "male"
+    Se o genero (gender) for feminino, utilize a palavra "female"
+*/
 
-// Verificar todos os critérios
-if (
-    (gender === "male" && barReps >= minBarReps && barSeconds <= maxBarSeconds) ||
-    (gender === "female" && abs >= minAbs) ||
-    runTime <= maxRunTime ||
-    swimTime <= maxSwimTime ||
-    diveTime <= maxDiveTime
-  ) {
-    passed = true;
-  }
-  
-  // Resultado
-  if (passed) {
-    console.log("Parabéns! Você passou no TAF.");
-  }
-  
-  console.log(passed);
+// **sua lógica a partir daqui**
+
+// Condições para candidatos masculinos
+if (gender === "male") {
+    // Teste de altura mínima
+    if (height >= 1.70) {
+        // Teste de repetições na barra ou tempo na barra
+        if (barReps >= 6 || barSeconds >= 15) {
+            // Teste de abdominais
+            if (abs >= 41) {
+                // Teste de corrida (pelo menos 3km em 12 minutos ou pelo menos 5km em 20 minutos)
+                if ((runDistance >= 3000 && runTime <= 720) || (runDistance >= 5000 && runTime <= 1200)) {
+                    // Teste de natação (pelo menos 100 metros em 1 minuto) ou mergulho (no máximo 30 segundos)
+                    if ((swimDistance >= 100 && swimTime <= 60) || diveTime <= 30) {
+                        passed = true;
+                    }
+                }
+            }
+        }
+    }
+} else if (gender === "female") {
+    // Teste de altura mínima
+    if (height >= 1.60) {
+        // Teste de repetições na barra ou tempo na barra
+        if (barReps >= 5 || barSeconds >= 12) {
+            // Teste de abdominais
+            if (abs >= 41) {
+                // Teste de corrida (pelo menos 4km em 15 minutos ou pelo menos 6km em 22 minutos)
+                if ((runDistance >= 4000 && runTime <= 900) || (runDistance >= 6000 && runTime <= 1320)) {
+                    // Teste de natação (pelo menos 100 metros em 1 minuto) ou mergulho (no máximo 30 segundos)
+                    if ((swimDistance >= 100 && swimTime <= 60) || diveTime <= 30) {
+                        passed = true;
+                    }
+                }
+            }
+        }
+    }
+}
+
+    /*
+        Seu código deve conter apenas UM console.log, e ele deve ser o abaixo.
+        Não altere nada pra baixo dessa linha, senão os testes não irão funcionar.
+    */
+        console.log(passed);
