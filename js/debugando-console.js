7116765f496859e9e83d9725fc6ad23323ab8a07
Mrprogramador <mrprogramador@outlook.com>
2023-06-21 16:21:20 -0400

    Finalizada |Atividade: Reutilizando Funções

--- js/debugando-console.js
+++ js/debugando-console.js
@@ -2,37 +2,49 @@
 //O depurador de código misteriosamente parou de funcionar, e agora você precisa descobrir o que deve ser retornado no console.log.
 
 function verificaNumero(num) {
-    let str = '';
-    
+
+    let paridade = '';
+
     if (num % 2 == 0) {
-      str = 'par';
+      paridade = 'par';
     } else {
-      str = 'impar';
+      paridade = 'impar';
     }
   
-    str += ', ';
+    let sinal = '';
   
     if (num > 0) {
-      str += 'positivo';
+      sinal = 'positivo';
     } else {
-      str += 'negativo';
+      sinal = 'negativo';
     }
   
-    str += ' e ';
+    let casasDecimais = '';
   
     if (num === parseInt(num)) {
-      str += 'não possui casas decimais';
+      casasDecimais = 'não possui casas decimais';
     } else {
-      str += 'possui casas decimais';
+      casasDecimais = 'possui casas decimais';
     }
   
-    str = 'O numero ' + num + ' é ' + str;
   
-    return str;
+    return `O número ${num} é ${paridade}, ${sinal} e ${casasDecimais}.`;
+  }
+  
+  console.log(verificaNumero(2.5));
+
+/*
+//Uma melhor forma de escrever esse código
+  function verificaNumero(num) {
+    let paridade = num % 2 === 0 ? 'par' : 'ímpar';
+    let sinal = num >= 0 ? 'positivo' : 'negativo';
+    let casasDecimais = Number.isInteger(num) ? 'não possui casas decimais' : 'possui casas decimais';
+  
+    return `O número ${num} é ${paridade}, ${sinal} e ${casasDecimais}.`;
   }
   
   console.log(verificaNumero(-12));
-  
+*/
   //Exercício 2
   //O trecho de código abaixo está realizando algumas operações matemáticas, e para se certificar de que todas elas estão funcionando como deveriam, o desenvolvedor adicionou alguns logs em seu código.
   
@@ -53,12 +65,32 @@ function verificaNumero(num) {
   }
   
   console.log(result);
-  
+ /*
+//Melhor forma de escrever o codigo
+  let valor = 18;
+  let result = valor / 2;
+  console.log(result);
+
+  result /= 3;
+  console.log(result);
+
+  let isPair = (result === 3);
+  console.log(isPair);
+
+  if (isPair) {
+    result *= 6;
+  } else {
+    result *= 12;
+  }
+
+  console.log(result);
+*/
+
   //Exercício 3
   //A função abaixo recebe um número qualquer e retorna a quantidade de algarismo que o compõem. Tente descobrir o valor que será retornado em cada console.log() sem rodar o código no terminal.
   
   function algarismosEmUmNumero(n){
-    let str = `${n}`;
+    let str = `${n}`; //convertente para string o valor do aprametros // n+""
     let contagem = str.length;
     console.log(contagem);
   
