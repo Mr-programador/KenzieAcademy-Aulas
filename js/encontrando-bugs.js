a2e8ee6f3147d9b5ace4aa96f4f77c0a17288a73
Mrprogramador <mrprogramador@outlook.com>
2023-06-22 00:40:40 -0400

    Finalizada |Atividade: Encontrando Bugs

--- js/encontrando-bugs.js
+++ js/encontrando-bugs.js
@@ -8,19 +8,40 @@ A função calculaIMC deve receber dois parâmetros, o peso em quilogramas e a a
 Dica: Fórmula para o cálculo é: IMC = peso / altura²
 */
 function calculaIMC(peso, altura) {
-  let alturaAoQuadrado = altura * altura;
-  let imc = peso / altura;
+  let alturaAoQuadrado = altura ** 2;
+  let imc = peso / alturaAoQuadrado;
+  let resultado = imc
+  return resultado
 }
 
-//Ao receber 80 para o peso e 1.80 para altura, a função deve retornar 24.691358024691358
-/*
-Exercício 2
-A função calculaIdade deve receber três parâmetros: um nome, anoFuturo, que representa um ano no futuro e anoNascimento, que representa o ano de nascimento. O objetivo dessa função é calcular a idade futura de uma pessoa tendo como base o ano de nascimento e o ano futuro.
-*/
-function calculaIdade(nome, anoFuturo, anoNascimento) {
-  let idade = anoNascimento + anoFuturo;
-  return "Em ${anoFuturo} $(nome) terá $(idade) anos de idade";
+console.log(calculaIMC(80, 1.80))
+
+/*/*Melhor forma de fazer a função.
+function calculaIMC(peso, altura) {
+  return peso / (altura * altura);
 }
+console.log(calculaIMC(80, 1.80));
+*/
+
+//Ao receber 80 para o peso e 1.80 para altura, a função deve retornar 24.691358024691358
+
+//Exercício 2
+//A função calculaIdade deve receber três parâmetros: um nome, anoFuturo, que representa um ano no futuro e anoNascimento, que representa o ano de nascimento. O objetivo dessa função é calcular a idade futura de uma pessoa tendo como base o ano de nascimento e o ano futuro.
+
+function calculaIdade(nome, anoFuturo, anoNascimento) {
+    let pessoa = nome;
+    let idade = anoNascimento - anoFuturo;
+    return `Em ${anoNascimento} ${pessoa} terá ${idade} anos de idade`;
+}
+console.log(calculaIdade('Pedro', 2002, 2030));
+
+/*Melhor forma de fazer a função.
+function calculaIdade(nome, anoFuturo, anoNascimento) {
+  let idade = anoNascimento - anoFuturo;
+  return `Em ${anoNascimento} ${nome} terá ${idade} anos de idade`;
+}
+console.log(calculaIdade('Maicon', 1995, 2030));
+
 
 //Ao receber os parâmetros: ("Pedro", 2002, 2030) a função deve retornar "Em 2030 Pedro terá 28 anos de idade".
 /*
@@ -29,20 +50,32 @@ A função algarismosEmUmNumero deve receber como parâmetro um número inteiro 
 Vale relembrar que ao realizarmos a conversão de um número para um texto, o ponto que divide a parte inteira da parte decimal é convertida junto, portanto, é necessário tratar caso o número apresente ponto flutuante.
 */
 function algarismosEmUmNumero(n) {
-  let str = `$(n)`;
-  let contagem = str.lenght;
+  let str = `${n}`;
+  let contagem = str.length;
 
   if (n != parseInt(n)) {
-    contagem = contagem + 1;
+    contagem = contagem - 1;
   }
 
-  return n;
+  return contagem;
 }
 
+console.log(algarismosEmUmNumero(3.14159265))
+
+/*Melhor forma de fazer a função.
+function algarismosEmUmNumero(n) {
+  let str = `${n}`;
+  // Remover o ponto decimal da string antes de calcular o comprimento
+  let contagem = str.replace('.', '').length;
+  return contagem;
+}
+console.log(algarismosEmUmNumero(3.14159265));
+
 //Ao receber 3.14159265 a função deve retornar 9.
-/*
+
 Exercício 4
 A função diaDaSemana deve receber por parâmetro um valor em formato de número ou texto, e retornar em texto o dia da semana que ele representa, seguindo a lista abaixo:
+
 Domingo
 Segunda-feira
 Terça-feira
@@ -50,32 +83,56 @@ Quarta-feira
 Quinta-feira
 Sexta-feira
 Sábado
+
 Caso o número recebido por parâmetro for inválido, você deve retornar o texto: 'Valor Inválido'.
 */
 function diaDaSemana(n) {
   let resultado = "";
 
-  if (n === 1 && n === '1') {
+  if (n === 1 || n === '1') {
     resultado = 'Domingo';
-  } else if(n == 2 ) {
+  } else if(n === 2 || n === '2') {
     resultado = 'Segunda-feira';
-  } else if(n == '3') {
+  } else if(n === 3 || n === '3') {
     resultado = 'Terça-feira';
   } else if(n === 4 || n === '4') {
     resultado = 'Quarta-feira';
-  } else if(n = 5) {
+  } else if(n === 5 || n === '5') {
     resultado = 'Quinta-feira';
-  } else if(n = '6') {
+  } else if(n === 6 || n === '6') {
     resultado = 'Sexta-feira'; 
-  } else(n == 7) {
+  } else if(n === 7 || n === '7') {
     resultado = 'Sábado';
-  } else if() {
+  } else{
     resultado = 'Valor Inválido';
   }
 
   return resultado;
 }
 
-//Ao receber '1' a função deve retornar 'Domingo'
-//Ao receber 3 a função deve retornar 'Terça-feira'
-//Ao receber 0 a função deve retornar 'Valor Inválido'
+console.log(diaDaSemana("1")); // Ao receber '1' a função deve retornar 'Domingo'
+console.log(diaDaSemana(3)); // Ao receber 3 a função deve retornar 'Terça-feira'
+console.log(diaDaSemana(0)); // Ao receber 0 a função deve retornar 'Valor Inválido'
+
+/*Melhor forma de fazer a função.
+function diaDaSemana(n) {
+  // Objeto que mapeia os números de 1 a 7 aos respectivos nomes dos dias da semana
+  const diasDaSemana = {
+    1: 'Domingo',
+    2: 'Segunda-feira',
+    3: 'Terça-feira',
+    4: 'Quarta-feira',
+    5: 'Quinta-feira',
+    6: 'Sexta-feira',
+    7: 'Sábado'
+  };
+
+  const numero = parseInt(n); // Converter o valor de entrada para um número
+
+  return diasDaSemana[numero] || 'Valor Inválido'; // Retorna o dia correspondente ou 'Valor Inválido' se não for encontrado
+}
+
+console.log(diaDaSemana("1")); // Ao receber '1' a função deve retornar 'Domingo'
+console.log(diaDaSemana(3)); // Ao receber 3 a função deve retornar 'Terça-feira'
+console.log(diaDaSemana(0)); // Ao receber 0 a função deve retornar 'Valor Inválido'
+*/
