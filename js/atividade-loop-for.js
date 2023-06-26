8330dfde81e7a8be290ffd5e2fdd4979b0349eba
Mrprogramador <mrprogramador@outlook.com>
2023-06-26 17:59:49 -0400

    Finalizada: Atividade: Loops for I

--- js/atividade-loop-for.js
+++ js/atividade-loop-for.js
@@ -3,55 +3,86 @@
 
 /*
 1) Escreva uma função chamada renderUpToTwenty. Quando a função for chamada, os números de 0 a 20 devem ser impressos no console.
-
 Chamada da função: renderUpToTwenty()
 saída: 0,1,2,3,4,5...20
 */
+function renderUpToTwenty() {
+  for (let num = 0; num <= 20; num++) {
+    console.log(num);
+  }
+}
+
+renderUpToTwenty(20);
 
 /*
 2) Escreva uma função chamada tenInTenToAHundred(). Quando a função for chamada, os números de 10 a 100, contando de 10 em 10, devem ser impressos no console.
-
-Exemplo:
 Chamada da função: ⁠tenInTenToAHundred()
 Saída: 10,20,30,40,50... 100
 */
+function tenInTenToAHundred() {
+  for (let i = 10; i <= 100; i += 10) {
+    console.log(i);
+  }
+}
+
+tenInTenToAHundred();
 
 /*
 3) Escreva uma função chamada oddUpToTwenty(). Quando a função for chamada, apresente no console os valores entre 0 e 20 que forem ímpares.
-
-Exemplo:
 Chamada da função: oddUpToTwenty()
 Saída: 1,3,5,7,9 ...19
 */
-
+function oddUpToTwenty() {
+  for (i = 1; i <= 20; i++) { //loop ate 20.
+    if (i % 2 !== 0) { //mostrando impares.
+      console.log(i);
+    }
+  }
+}
+oddUpToTwenty();
 /*
 4) Escreva uma função chamada evenUpToTwenty(). Quando a função for chamada, apresente no console os valores entre 0 e 20 que forem pares.
-
-Exemplo:
 Chamada da função: evenUpToTwenty()
 Saída: 0,2,4,6,8 ...20
 */
-
+function evenUpToTwenty(){
+  for(i = 0; i <= 20; i++){//loop ate 20.
+    if(i%2 == 0){//mostrando impares.
+      console.log(i);
+    }
+  }
+}
+evenUpToTwenty();
 /*
 5) Escreva uma função chamada fromNegativeToPositive(). Quando a função for chamada, apresente os valores de -10 a 0.
-
-Exemplo:
 Chamada da função: fromNegativeToPositive()
 Saída: -10,-9,-8,-7 ...0
 */
-
+function fromNegativeToPositive(){
+  for(i = -10; i <= 0; i++){
+    console.log(i);
+  }
+}
+fromNegativeToPositive()
 /*
 6) Escreva uma função chamada inDescendingDirection. Quando a função for chamada, apresente valores de 10 a 0 no console.
-
-Exemplo:
 Chamada da função: inDescendingDirection()
 Saída: 10,9,8,7...0
 */
-
+function inDescendingDirection(){
+  for (i = 10; i >= 0; i--){
+    console.log(i);
+  }
+}
+inDescendingDirection();
 /*
 7) Escreva uma função chamada toSquare(). Quando a função for chamada, apresente o quadrado dos valores da sequência de 1 a 10.
-
-Exemplo:
 Chamada da função: toSquare()
 Saída: 1,4,9,16...100
 */
+function toSquare(){
+  for(i = 1; i <= 10; i++){
+    console.log(i*i);
+  }
+}
+toSquare();
---
+++ js/atividade-loop-for2.js
@@ -0,0 +1,34 @@
+//Introdução
+/*
+Nesta lista de exercícios você desenvolverá suas habilidades usando os conceitos aprendidos sobre a estrutura de repetição for.Você deverá desenvolver uma função para cada exercício. Dê nomes para as funções de acordo com suas funcionalidades.
+
+Escreva uma função que apresente como resultado os quadrados dos números inteiros existentes na faixa de valores de 15 a 200.
+chamada da função: nome_da_sua_funcao()
+Saída: 225, 256, 289 ...40000
+*/
+function quadraDosNumeros(){
+    for(let i = 15; i <= 200; i++){
+        console.log(i*i);
+    }
+}
+quadraDosNumeros();
+/*
+Escreva uma função que apresente a soma dos cem primeiros números naturais.
+(1+2+3+...+98+99+100).
+chamada da função: nome_da_sua_funcao()
+Saída: 5.050
+*/
+
+/*
+Escreva uma função que apresente todos os valores numéricos menores que 200 que são divisíveis por 4.
+chamada da função: nome_da_sua_funcao()
+Saída: 0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40 ...196
+*/
+
+/*
+Escreva uma função que calcule o resultado da soma e da média aritmética dos valores pares situados na faixa numérica de 50 até 70.
+soma = 50+52+54...+70
+média_aritmética = soma / qtd_numeros_pares_no_intervalo⁠⁠
+chamada da função: funcao_o_nome_da_sua_funcao()
+Saída: A soma é 660 e a média é 60.
+*/
