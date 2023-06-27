5e0ebb13b8fb62346d14eb25013f6578942171e5
Mrprogramador <mrprogramador@outlook.com>
2023-06-26 21:40:35 -0400

    Finalizada: Atividade: Loops for II

--- js/atividade-loop-for2.js
+++ js/atividade-loop-for2.js
@@ -6,24 +6,39 @@ Escreva uma função que apresente como resultado os quadrados dos números inte
 chamada da função: nome_da_sua_funcao()
 Saída: 225, 256, 289 ...40000
 */
-function quadraDosNumeros(){
+function quadrados_dos_numeros(){
     for(let i = 15; i <= 200; i++){
         console.log(i*i);
     }
 }
-quadraDosNumeros();
+quadrados_dos_numeros();
 /*
 Escreva uma função que apresente a soma dos cem primeiros números naturais.
 (1+2+3+...+98+99+100).
 chamada da função: nome_da_sua_funcao()
 Saída: 5.050
 */
-
+function soma_dos_cem(){
+    let soma = 0;
+    for(let i = 0; i <= 100; i++){
+        soma += i;
+        console.log(soma);
+    }
+}
+soma_dos_cem();
 /*
 Escreva uma função que apresente todos os valores numéricos menores que 200 que são divisíveis por 4.
 chamada da função: nome_da_sua_funcao()
 Saída: 0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40 ...196
 */
+function divide_por_quatro(){
+    for(let i = 0; i < 200; i++){
+        if(i % 4 == 0){
+            console.log(i);
+        }
+    }
+}
+divide_por_quatro();
 
 /*
 Escreva uma função que calcule o resultado da soma e da média aritmética dos valores pares situados na faixa numérica de 50 até 70.
@@ -32,3 +47,27 @@ média_aritmética = soma / qtd_numeros_pares_no_intervalo⁠⁠
 chamada da função: funcao_o_nome_da_sua_funcao()
 Saída: A soma é 660 e a média é 60.
 */
+
+function calcularSomaEMedia() {
+    let soma = 0;
+    let quantidadePares = 0;
+  
+    for (let i = 50; i <= 70; i += 2) {
+      soma += i;
+      quantidadePares++;
+    }
+  
+    let media = soma / quantidadePares;
+  
+    console.log("A soma é", soma, "e a média é", media.toFixed(2));
+  }
+  
+  calcularSomaEMedia();
+
+  
+  
+  
+  
+  
+  
+  
